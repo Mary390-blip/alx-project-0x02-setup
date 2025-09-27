6d4fe11b480523f5f5f2dd70247831c5cd77c920
Mary Hellen Otinga <maryotinga98@gmail.com>
2025-09-27 11:07:30 +0400

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -16,3 +16,8 @@ export interface CardProps {
   title: string;
   content: string;
 }
+export interface ButtonProps {
+  title: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+}
